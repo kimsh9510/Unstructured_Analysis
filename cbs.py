084760ea17b2b860acb43160a0c6e5f2e7a62322
kimsh9510 <kimsh9510@naver.com>
2022-03-25 18:25:00 +0900

    connect cbs message and keyword

--- cbs.py
+++ cbs.py
@@ -1,7 +1,7 @@
 # 라이브러리 import
 import requests
 import xml.etree.ElementTree as ET
-
+from konlpy.tag import Okt
 
 url = 'http://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List?serviceKey=vBWCvCxW515%2BgkLcMNXIXHBLHaNh9Xa8F8V9TrjFpKFeB0lP7D9%2BwXEIuAttbGQPrND%2BREAx2GIJf5eXoHF%2FdA%3D%3D&pageNo=1&numOfRows=10&type=xml'
 response = requests.get(url)
@@ -9,13 +9,20 @@ contents = response.text
 xml = ET.fromstring(contents)
 
 for message in xml.findall("row"):
+    print("here")
     create_date = message.find("create_date")  # 생성일시
     location_id = message.find("location_id")  # 수신지역 코드
     location_name = message.find("location_name")  # 수신지역 이름
     md101_sn = message.find("md101_sn")  # 일련번호
     msg = message.find("msg")  # 내용
     send_platform = message.find("send_platform")  # 발신처
-    print(create_date.text)
+
+tagger=Okt()
+#part_of_speech = tagger.pos('[영동군청]코로나19 확진 159명(확진자접촉 56, 증상발현 101, 무증상 2) ▲검사 실시.')
+#print(part_of_speech)  # 튜플 형태로 출력
+noun_list = tagger.nouns(msg.text)
+print(noun_list)  # 리스트 형식으로 출력
+
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from neo4j import __version__ as neo4j_version
 from neo4j import GraphDatabase
 import requests
 import xml.etree.ElementTree as ET
+from konlpy.tag import Okt
 
 print(neo4j_version)
 
@@ -41,8 +42,9 @@ if __name__ == "__main__":
     url = 'http://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List?serviceKey=vBWCvCxW515%2BgkLcMNXIXHBLHaNh9Xa8F8V9TrjFpKFeB0lP7D9%2BwXEIuAttbGQPrND%2BREAx2GIJf5eXoHF%2FdA%3D%3D&pageNo=1&numOfRows=10&type=xml'
     response = requests.get(url)
     contents = response.text
-    print(contents)
+    #print(contents)
     xml = ET.fromstring(contents)
+    tagger = Okt()
 
     for message in xml.findall("row"):
         create_date = message.find("create_date").text  # 생성일시
@@ -51,6 +53,21 @@ if __name__ == "__main__":
         md101_sn = message.find("md101_sn").text # 일련번호
         msg = message.find("msg").text  # 내용
         send_platform = message.find("send_platform").text  # 발신처
+
+        #쿼리 입력
         conn.query("create (a1:CBS {date: '"+create_date+"', location_id: '"+location_id+"', location_name: '"+location_name+"', md101_sn: '"+md101_sn+"', msg: '"+msg+"', send_platform: '"+send_platform+"'})")
         #conn.query("create (a1: CBS {date : '"+create_date+"'})")
         #conn.query("match (a) optional match (a)-[r]-() delete a, r")
+
+        #각 msg에 대한 keyword 추출 후 KEYWORD 쿼리 생성
+        noun_list = tagger.nouns(msg)
+        for keyword in noun_list:
+            conn.query("create (a2:KEYWORD {msg: '" + msg + "', keyword: '" + keyword + "'})")
+            print("finish")
+
+        #각 cbs메시지와 키워드 간의 match
+        conn.query("match(a1:CBS{msg:'" + msg + "'}),(a2:KEYWORD{msg:'" + msg + "'})create(a1)-[r:keyword]->(a2)")
+
+
+
+
